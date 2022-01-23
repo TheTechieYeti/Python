c7bd62015e990f0a251f81d9a9578879a87fa676
TheTechieYeti <elliott.scott.t@gmail.com>
2022-01-23 13:32:45 -0800

    completed ninja and sensai challenges

--- fundamentals/oop/Stoes & Products/product.py
+++ fundamentals/oop/Stoes & Products/product.py
@@ -1,23 +1,30 @@
 
 class Product:
-
+    id = 1
     def __init__(self, name, price, category, quantity = 1):
         self.name = name
         self.price = price
         self.category = category
         self.quantity = quantity
-
+        self.id = Product.id
+        Product.id += 1
+    def __repr__(self):
+        return self.name
+    
     def update_price(self, percent_change, is_increased):
         if is_increased == True:
             self.price += (self.price * percent_change)
-            print(f"New Price is {self.price}.")
+            print(f"{self.name}'s new price is {self.price}.")
             return self
 
         if is_increased == False:
             self.price -= (self.price * percent_change)
-            print(f"New Price is {self.price}.")
+            print(f"{self.name}'s new price is {self.price}.")
             return self
     
     def print_info(self):
-        print(f"Name: {self.name} \n Price: {self.price} \n Category: {self.category} \n Quantity: {self.quantity}")
+        print(f" Id: {self.id} Name: {self.name} Price: {self.price} Category: {self.category} Quantity: {self.quantity}")
         return self
+
+
+
--- fundamentals/oop/Stoes & Products/store.py
+++ fundamentals/oop/Stoes & Products/store.py
@@ -1,22 +1,39 @@
-### Things to work on ###
-#1 - How to print values, and not memory locations. Everytime I try to print the list of products from the store, I get a memory location
-#2 - Change the inflation method and set_clearance method to the Store CLass 
-#3 - Update product class to give each product a unique id. Update the sell_product method to accept the unique id. 
 
 
+
+import re
 import product
 
 class Store:
+    @classmethod
+    def inflation(cls, percent_increase):
+        for i in range(len(cls.products)):
+            product.Product.update_price(cls.products[i], percent_increase, True)
+        
+    @classmethod
+    def clearance(cls, percent_discount):  
+        for i in range(len(cls.products)):
+            if cls.products[i].price <= 0:
+                print(f"{cls.products[i].name} item is already free.")
+                continue
+            product.Product.update_price(cls.products[i], percent_discount, False)
+        return
+
     def __init__(self, name, products = []):
         self.name = name
         self.products = products
 
     def __str__(self):
-        return self.products
+        return str(self.products)
 
-    def inventory(self): ### Not chainable ###
-            inventory = self.products
-            return inventory
+    def inventory(self):
+            print(self.products)
+            return self
+    
+    def inventory1(self):
+        for i in range(len(self.products)):
+            product.Product.print_info(self.products[i])
+        return self
 
     def add_product(self, new_product):
         for i in range(len(self.products)):
@@ -24,11 +41,14 @@ class Store:
                 print(f"Item is already for sale. Please choose a different product to add.")
                 return self
         self.products.append(new_product)
-        print(Store.inventory(self))
+        # print(Store.inventory(self))
         return self
 
     def sell_product(self, id):
-        self.products.remove(id)
+        for i in range(len(self.products)-1):
+             if id == self.products[i].id:
+                print("found it")
+                self.products.pop(i)
         print(self.products)
         return self
     
@@ -37,15 +57,20 @@ class Store:
             product.Product.update_price(self.products[i], percent_increase, True)
         return self
     
-    def clearance(self, percent_discount):  ## How can I do an iterave if statement that compares the cost of each product in my store to see if it is $0.00?
+    def clearance(self, percent_discount):  
         for i in range(len(self.products)):
+            if self.products[i].price <= 0:
+                print(f"{self.products[i].name} item is already free.")
+                continue
             product.Product.update_price(self.products[i], percent_discount, False)
         return self
 
 Heritage = Store("heritage")
-Espresso = product.Product("Espresso", 3.50, "Drink")
+Espresso = product.Product("Espresso", 3, "Drink")
 Halie_Awesome = product.Product("Halie Awesome", 8.00, 'Food')
 Coffee_Beans = product.Product("Coffee Beans", 18.00, "Goods")
+Sticker = product.Product("Sticker", 0, "Goods")
 
-Heritage.add_product(Espresso).add_product(Halie_Awesome).inflation(.1).clearance(.1)
-print(Store.inventory(Heritage))
+Heritage.add_product(Espresso).add_product(Halie_Awesome).add_product(Coffee_Beans).add_product(product.Product("Test", .00, "Goods")).inventory1().sell_product(1)
+Heritage.inflation(.5)
+Heritage.clearance(.5)
