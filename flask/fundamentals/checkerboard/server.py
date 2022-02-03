1dd46ed7062493836ee83d3d7c54f2e2fd5c9d19
TheTechieYeti <elliott.scott.t@gmail.com>
2022-02-03 15:26:46 -0800

    Completed Assignment

--- flask/fundamentals/checkerboard/server.py
+++ flask/fundamentals/checkerboard/server.py
@@ -7,13 +7,13 @@ def index():
 def rows(rows = 8, ):
     return render_template("rows.html", rows = rows,)
 
-@app.route('/<int:rows>/<int:cols>')
-def rows_cols(rows, cols):
-    return render_template('rows_cols.html', rows = rows, cols = cols)
+# @app.route('/<int:rows>/<int:cols>')
+# def rows_cols(rows, cols):
+#     return render_template('rows_cols.html', rows = rows, cols = cols)
 
-@app.route('/<int:rows>/<int:cols>/<color1>/<color2>')
-def rows_cols_colors(rows, cols, color1, color2):
-    return render_template('rows_cols_colors.html', rows = rows, cols = cols, color1 = color1, color2 = color2)
+# @app.route('/<int:rows>/<int:cols>/<color1>/<color2>')
+# def rows_cols_colors(rows, cols, color1, color2):
+#     return render_template('rows_cols_colors.html', rows = rows, cols = cols, color1 = color1, color2 = color2)
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
