9815693ace96894e8ad6a6ef982b4451a45badfa
TheTechieYeti <elliott.scott.t@gmail.com>
2022-02-08 09:31:14 -0800

    Completed Assignment

--- flask_mysql/db_connection/users/server.py
+++ flask_mysql/db_connection/users/server.py
@@ -3,25 +3,66 @@ from user import User
 app = Flask(__name__)  
 app.secret_key ='testsecretkey'
 
-@app.route('/')         
+@app.route('/users')         
 def index():
     users = User.get_all()
     return render_template("index.html", users=users)
 
-@app.route('/new')         
+@app.route('/users/new')         
 def new():
     return render_template("new.html")
 
-@app.route('/process', methods = ["POST"])         
+@app.route('/users/new/process', methods = ["POST"])         
 def process():
     data = {
         "fname": request.form["fname"],
         "lname": request.form["lname"],
         "email": request.form["email"],
     }
-    User.save(data)
-    return redirect('/')
+    userid = User.save(data)  ###Why does this store my user id? Why is the return value of User.save(data) the user id? 
+    print(f"process test {userid}")
+    return redirect(f'/users/{userid}')
 
+@app.route('/users/<int:userid>')
+def show(userid):
+    data ={
+        "id": userid
+    }
+    user_from_database = User.get_one(data)
+    print(f"DB USER: {user_from_database}")
+    print(type(user_from_database))
+    return render_template("user.html", user=user_from_database)
+
+@app.route('/users/<int:userid>/edit')         
+def edit(userid): ## Our id from the browser gets passed into our function as a parameter
+    data ={
+        "id":userid  # key of id = value from our browser.
+    }
+    user_from_database = User.get_one(data) ## This variable equals the value of the class method we called. The class method returned value is one instantiation of a User class that has all the attributes of the corresponding tables from our data base. 
+    print(f"DB USER: {user_from_database}")
+    print(type(user_from_database))
+    return render_template("edit.html", user=user_from_database) ## This is returning the our instance of User Class as a variable 'user'
+
+@app.route('/users/<int:userid>/edit/update', methods=["POST"])         
+def editupdate(userid):
+    data = {
+        "id": userid,
+        "fname": request.form["fname"],
+        "lname": request.form["lname"],
+        "email": request.form["email"]
+    }
+    print(f"This is the data to be updated {data}")
+    User.update(data)
+    return redirect('/users')
+
+@app.route('/users/<int:userid>/delete')         
+def delete(userid):
+    data = {
+        "id": userid,
+    }
+    print(f"User {data} is about to be deleted.")
+    User.delete(data)
+    return redirect('/users')
 
 @app.route('/reset')
 def reset():
--- flask_mysql/db_connection/users/user.py
+++ flask_mysql/db_connection/users/user.py
@@ -21,10 +21,36 @@ class User:
         for user in results:
             users.append( cls(user) )
         return users
-        
+    @classmethod
+    def get_one(cls, data):
+        query = "SELECT * FROM users where id = %(id)s ;"
+        # make sure to call the connectToMySQL function with the schema you are targeting.
+        results = connectToMySQL('users_schema').query_db(query, data)  ##We connect first to the function, they query the database using the variable query, and our data which contains our corresponding ID for the user we want. Results is a LIST of dictionarys
+        print(f"RESULTS: {results}")
+        print(type(results[0]))
+        this_user = cls(results[0]) #Here is where we create an instance of user class with the list "results" which contains a dictionary of all the keys and values we need and store it in a variable
+        print(this_user)
+        print(type(this_user))
+        return this_user #We return the variable that stores the instance of the user class we created so when we call that function elsewhere, we get that instance. 
+
     @classmethod
     def save(cls, data):
         query = "INSERT INTO users (first_name, last_name, email, created_at, updated_at) VALUES( %(fname)s, %(lname)s, %(email)s, NOW(), NOW() );"
         print("user information stored")
         return connectToMySQL('users_schema').query_db(query, data)
+
+    @classmethod
+    def update(cls, data):  ####### Can't get this to work with updated_at = Now()
+        query = "UPDATE users SET first_name = %(fname)s, last_name = %(lname)s, email = %(email)s  WHERE id = %(id)s ;"
+        print("user information updated")
+        return connectToMySQL('users_schema').query_db(query,data)
+
+    @classmethod
+    def delete(cls, data):
+        query = "DELETE from users WHERE id = %(id)s"
+        return connectToMySQL('users_schema').query_db(query,data)
+
+    def fullname(self,):
+        fullname = f"{self.first_name} {self.last_name}"
+        return fullname
             
